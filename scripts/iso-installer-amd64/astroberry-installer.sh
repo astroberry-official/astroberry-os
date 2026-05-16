2bf5d3e732fded5dad1fe518551acb308eb42add
Radek Kaczorek <rkaczorek@gmail.com>
2026-05-16 19:33:38 +0200

    Add disk selection dialog and update installer path to /usr/bin

--- scripts/iso-installer-amd64/astroberry-installer.sh
+++ scripts/iso-installer-amd64/astroberry-installer.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+#
+# astroberry-installer.sh
+# Install Astroberry OS to a disk
+# Invoke from the live ISO by running:
+#     sudo /usr/bin/astroberry-installer.sh
+
 set -e
 
-# 1. Hardware (disk) Detection
-TARGET_DRIVE=$(lsblk -bndo NAME,TYPE,RO,RM,SIZE | \
-    awk '$2=="disk" && $3=="0" && $4=="0" {print $1, $5}' | \
-    sort -rnk2 | head -n1 | awk '{print "/dev/"$1}')
+TITLE="Astroberry OS Installer"
 
-if [ -z "$TARGET_DRIVE" ]; then
-    zenity --error --text="No suitable target drive found!"
+# 1. Hardware (disk) Selection
+DRIVES=$(lsblk -dnpo NAME,TYPE,RO,RM,SIZE,MODEL | awk '$2=="disk" && $3=="0" && $4=="0" {print $1"|"$5"|"$6"-"$7}' | awk -F\| '{ORS=" "; print $1, $3, $2}; ORS="\n"')
+DRIVES=( ${DRIVES[@]/#/"FALSE "} )
+
+TARGET_DRIVE=$(zenity --width 600 --height 400 --title="$TITLE" --text="Select destination disk :" --list --radiolist --column "Disk" --column="Device" --column="Model" --column "Size" "${DRIVES[@]}")
+
+if [ -z "$TARGET_DRIVE" ] || [ ! -e "$TARGET_DRIVE" ]; then
+    zenity --error --text="You have to select a disk for installation!"
     exit 1
 fi
 
@@ -15,12 +24,12 @@ DISK_SIZE=$(lsblk -dn -o SIZE "$TARGET_DRIVE")
 DISK_MODEL=$(cat "/sys/block/${TARGET_DRIVE#/dev/}/device/model" 2>/dev/null || echo "Virtual Disk")
 
 # 2. GUI Confirmation
-zenity --question --title="Astroberry Live Installer" --width=450 \
+zenity --question --title="$TITLE" --width=450 \
 --text="<b>Installation Summary</b>\n\nTarget: $TARGET_DRIVE ($DISK_MODEL)\nSize: $DISK_SIZE\n\n<span foreground='red'><b>WARNING: This will wipe $TARGET_DRIVE.</b></span>\n\nProceed?" || exit 1
 
 # 3. Processing
 (
-echo "5" ; echo "# Partitioning $TARGET_DRIVE..."
+echo "5" ; echo "# Partitioning disk $TARGET_DRIVE..."
 [[ $TARGET_DRIVE == *nvme* ]] && PART_EXT="p" || PART_EXT=""
 EFI_PART="${TARGET_DRIVE}${PART_EXT}1"
 ROOT_PART="${TARGET_DRIVE}${PART_EXT}2"
@@ -45,13 +54,13 @@ sudo mkdir -p /mnt/target/boot/efi
 sudo mount "$EFI_PART" /mnt/target/boot/efi
 
 # Cloning live system
-echo "30" ; echo "# Cloning live system (this may take a few minutes)..."
+echo "30" ; echo "# Copying system files (this may take a few minutes)..."
 sudo rsync -aAX --progress \
     --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.cache/*"} \
     / /mnt/target
 
 # Creating fstab (the delayed copy is because I need to use sudo)
-echo "80" ; echo "# Configuring hardware IDs (fstab)..."
+echo "80" ; echo "# Configuring system..."
 ROOT_UUID=$(lsblk -dn -o UUID "$ROOT_PART")
 EFI_UUID=$(lsblk -dn -o UUID "$EFI_PART")
 cat <<EOF > "/tmp/fstab"
@@ -61,7 +70,7 @@ EOF
 sudo cp /tmp/fstab /mnt/target/etc/fstab
 
 # Setting up bootloader (Chroot)
-echo "85" ; echo "# Setting up bootloader (Chroot)..."
+echo "85" ; echo "# Setting up bootloader..."
 sudo mount --bind /dev /mnt/target/dev
 sudo mount --bind /proc /mnt/target/proc
 sudo mount --bind /sys /mnt/target/sys
@@ -82,7 +91,7 @@ sudo chroot /mnt/target /bin/bash -c "
 "
 
 # Unmounting and finalizing
-echo "95" ; echo "# Unmounting and finalizing..."
+echo "95" ; echo "# Finalizing..."
 if [ $BOOT_MODE == "uefi" ]; then
     sudo umount /mnt/target/sys/firmware/efi/efivars
 fi
@@ -92,7 +101,7 @@ sudo umount /mnt/target
 sudo sync
 
 echo "100" ; echo "# Done!"
-) | zenity --progress --title="Astroberry OS Installation" --percentage=0 --auto-close --no-cancel
+) | zenity --progress --title="$TITLE" --percentage=0 --auto-close --no-cancel
 
 if zenity --question --text="Installation successful! Reboot now?" ; then
     reboot
